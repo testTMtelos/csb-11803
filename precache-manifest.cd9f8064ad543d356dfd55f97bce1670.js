211da686b1616b130f1f4690551ddac7bb69697d
Code Sandbox <hello@codesandbox.io>
2021-12-08 00:48:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.cd9f8064ad543d356dfd55f97bce1670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea0a2b8022d620eb78c61f5de77a736",
+    "url": "https://testtmtelos.github.io/csb-11803/index.html"
+  },
+  {
+    "revision": "aba4a13f8834d68ec405",
+    "url": "https://testtmtelos.github.io/csb-11803/static/css/main.cca60e33.chunk.css"
+  },
+  {
+    "revision": "93c5c76c524bedb840c3",
+    "url": "https://testtmtelos.github.io/csb-11803/static/js/2.1f1ae746.chunk.js"
+  },
+  {
+    "revision": "23b26446db404c4cd1d2ae5ba4f80a38",
+    "url": "https://testtmtelos.github.io/csb-11803/static/js/2.1f1ae746.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aba4a13f8834d68ec405",
+    "url": "https://testtmtelos.github.io/csb-11803/static/js/main.52fce834.chunk.js"
+  },
+  {
+    "revision": "49431c9f2f7a0433de0f",
+    "url": "https://testtmtelos.github.io/csb-11803/static/js/runtime-main.e92bcdda.js"
+  }
+]);
